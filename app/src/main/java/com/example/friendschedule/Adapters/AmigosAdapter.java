13fa012f7835c600ef7d7c53c976b54810fbe882
BrVenturaU <brandonventura16@gmail.com>
2020-09-17 23:12:42 -0600

    Configura Adaptador

--- app/src/main/java/com/example/friendschedule/Adapters/AmigosAdapter.java
+++ app/src/main/java/com/example/friendschedule/Adapters/AmigosAdapter.java
@@ -1,14 +1,18 @@
 package com.example.friendschedule.Adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.friendschedule.Entities.Amigo;
 import com.example.friendschedule.Interfaces.IAmigoService;
+import com.example.friendschedule.R;
 
 import java.util.ArrayList;
 
@@ -29,29 +33,51 @@ public class AmigosAdapter extends RecyclerView.Adapter<AmigosAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView tvNombreCompleto, tvTelefono;
+        ImageButton btnFavorito;
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            tvNombreCompleto = itemView.findViewById(R.id.tvNombreCompleto);
+            tvTelefono = itemView.findViewById(R.id.tvTelefono);
+            btnFavorito = itemView.findViewById(R.id.btnFavorito);
         }
     }
 
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.item_amigo, parent, false);
+        view.setOnClickListener(this);
+        ViewHolder viewHolder = new ViewHolder(view);
+
+        return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-
+        Amigo amigo = amigos.get(position);
+        setAmigoData(amigo, holder);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return amigos.size();
     }
 
     @Override
     public void onClick(View view) {
+        if(listener != null)
+            listener.onClick(view);
+    }
 
+    public void setAmigoData(Amigo amigo, ViewHolder holder){
+        holder.tvNombreCompleto.setText(amigo.getPrimerNombre() + " " + amigo.getPrimerApellido());
+        holder.tvTelefono.setText(amigo.getTelefono());
+        if(amigo.getEsFavorito())
+            holder.btnFavorito.setImageResource(R.drawable.ic_favorite_added);
+        else
+            holder.btnFavorito.setImageResource(R.drawable.ic_favorite_remove);
     }
 }
