1b38bf02fbe5f428e9a7f1ba89993e3cd06ba9c0
BrVenturaU <brandonventura16@gmail.com>
2020-09-19 22:29:54 -0600

    Agrega informacion al layotu

--- app/src/main/java/com/example/friendschedule/InformacionAmigoActivity.java
+++ app/src/main/java/com/example/friendschedule/InformacionAmigoActivity.java
@@ -3,6 +3,7 @@ package com.example.friendschedule;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.friendschedule.Entities.Amigo;
@@ -13,6 +14,7 @@ public class InformacionAmigoActivity extends AppCompatActivity {
 
     private Amigo amigo;
     private IAmigoService amigoService;
+    private TextView tvNombreCompleto, tvTelefono, tvEmail, tvFechaNacimiento;
 
     //TODO Mostrar datos en la pantalla
     //TODO Permitir agregar a favoritos
@@ -25,7 +27,19 @@ public class InformacionAmigoActivity extends AppCompatActivity {
         final int idAmigo =getIntent().getExtras().getInt("idAmigo");
         amigoService = new AmigoService();
         amigo = amigoService.getById(InformacionAmigoActivity.this, idAmigo);
-        Toast.makeText(InformacionAmigoActivity.this, amigo.getPrimerNombre(), Toast.LENGTH_SHORT).show();
+
+        tvNombreCompleto = findViewById(R.id.tvNombreCompleto);
+        tvTelefono = findViewById(R.id.tvTelefono);
+        tvEmail = findViewById(R.id.tvEmail);
+        tvFechaNacimiento = findViewById(R.id.tvFechaNacimiento);
+
+        String nombreCompleto = amigo.getPrimerNombre() + " " + amigo.getSegundoNombre() + " " + amigo.getPrimerApellido() + " " + amigo.getSegundoApellido();
+
+        tvNombreCompleto.setText(nombreCompleto);
+        tvTelefono.setText(amigo.getTelefono());
+        tvEmail.setText(amigo.getEmail());
+        tvFechaNacimiento.setText(amigo.getFechaNacimiento().toString());
+
 
     }
 }
